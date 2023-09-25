fd7a57d7b4d6c5fde3d798792c7f65bb79e28feb
korsary4 <60779176+korsary4@users.noreply.github.com>
2023-09-26 01:23:00 +0300

    Delete shop directory

--- shop/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Review
-
-class ReviewForm(forms.Form):
-    rating = forms.IntegerField(label='Рейтинг', min_value=1, max_value=5)
-    comment = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows': 5, 'cols': 60}))
